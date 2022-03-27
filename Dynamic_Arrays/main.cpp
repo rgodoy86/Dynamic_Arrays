8b3822482c1bafc7cfea7a7c03ed53c0effeb97d
rgodoy86 <rgodoy86@gmail.com>
2022-03-27 19:53:18 +0200

    Creation of Multidimensional dynamic array

--- Dynamic_Arrays/main.cpp
+++ Dynamic_Arrays/main.cpp
@@ -25,15 +25,47 @@ int main() {
     }
     
     // Reading and printing the values from the array
+    cout << "Unidimensional Dynamic Array" << endl;
     for (int r = 0; r < rows; ++r){
         cout << "array[" << r << "]: " << array[r] << endl;
     }
+    cout << endl;
     
     // Deallocating
     delete [] array;
     array = NULL;
 
     // // Multidimensional Dynamic Array
+    // Allocating
+    int **table = new int*[rows];
+    for (int r = 0; r < rows; ++r){
+        table[r] = new int[columns];
+    }
+    
+    // Assigning values to the array
+    for(int r = 0; r < rows; ++r){
+        for(int c=0; c < columns; ++c){
+            table[r][c] = ((10 - (c + 1)) * 10) + c;
+        }
+    }
+    // Reading and printing the values from the array
+    cout << "Multidimensional Dynamic Array" << endl;
+    for(int c = 0; c < columns; ++c){
+        for(int r=0; r < rows; ++r){
+            cout << "Table[" << r << "][" << c << "]: " << table[r][c] << endl;
+        }
+    }
+    cout << endl;
+    
+    // Deallocating
+    for(int r=0; r > rows; ++r){
+        delete [] table[r];
+        table[r] = NULL;
+    }
+    delete [] table;
+    table = NULL;
+    
+    
     
     return 0;
 }
