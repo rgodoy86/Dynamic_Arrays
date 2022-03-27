5a41c4d6fdf03c0faf393f23330d89f8ff6afdaf
rgodoy86 <rgodoy86@gmail.com>
2022-03-27 19:12:21 +0200

    Initial Commit

---
+++ Dynamic_Arrays/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Dynamic_Arrays
+//
+//  Created by Raphael Godoy on 27.03.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
