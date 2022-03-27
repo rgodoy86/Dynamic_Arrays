ee7ee9f6aa9a9346bd8b08a032dcbf62114292ae
rgodoy86 <rgodoy86@gmail.com>
2022-03-27 19:37:32 +0200

    Unidimensional dynamic array

--- Dynamic_Arrays/main.cpp
+++ Dynamic_Arrays/main.cpp
@@ -6,9 +6,34 @@
 //
 
 #include <iostream>
+#include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+    
+    int rows = 5;
+    int columns = 5;
+    
+    // // Unidimensional Dynamic Array
+    // Allocating
+    int * array = new int[rows];
+    
+    // Assigning values to the array
+    for (int r = 0; r < rows; ++r){
+        array[r] = (r + 1) * 10;
+    }
+    
+    // Reading and printing the values from the array
+    for (int r = 0; r < rows; ++r){
+        cout << "array[" << r << "]: " << array[r] << endl;
+    }
+    
+    // Deallocating
+    delete [] array;
+    array = NULL;
+
+    // // Multidimensional Dynamic Array
+    
     return 0;
 }
